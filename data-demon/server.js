d57b5ed4b4995d637c71f75064239d289df4bfec
Khaled Al K <57811573+kkodmani@users.noreply.github.com>
2023-03-29 11:00:28 -0400

    Working producer and consumer in the server
    Still needs work to do be done, committing for safety

--- data-demon/server.js
+++ data-demon/server.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
+const produce = require("./producer");
+const rawdata = require("./sample.json");
+const APIKey = 'c9c5839bd781b1c418c23e5d0df9b0fe';
 // Middleware
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Data Demon');
+app.get("/", async (req, res) => {
+ // res.send("Data Demon");
+  let city = req.query.city;
+   fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      produce("w/e", JSON.stringify(data), (durable = false));
+    res.send(`The weather in ${city} is ${data.main.temp} degrees Celsius`);
+    });
 });
+
+const sampleData = JSON.stringify(rawdata);
+// produce("w/e", sampleData, (durable = false));
 
 // Start the server
 const port = process.env.PORT || 3000;
