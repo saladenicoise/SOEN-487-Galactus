c7a9a3d11f415e3fb6d1deca89b706c1054bc117
Quoiquecesoit <90417180+Quoiquecesoit@users.noreply.github.com>
2023-03-31 11:01:00 -0400

    small detail

--- sky-siren/src/App.vue
+++ sky-siren/src/App.vue
@@ -49,7 +49,7 @@
 
   	<router-view></router-view>
 
-	<footer>
+	<footer class="text-center">
 		<div class="container " >
 			<span class="text-muted"> Made with <i class="bi bi-heart-fill text-danger"></i> by L'equipe<br> </span>
 
