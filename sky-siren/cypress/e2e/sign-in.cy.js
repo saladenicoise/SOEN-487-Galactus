df0ebb098bfa227efb111227a33e742eee4c58b6
Jaskaran Dhadda <jaskaran.dhadda@bayardcanada.com>
2023-04-01 19:06:26 -0400

    Added sign in test

---
+++ sky-siren/cypress/e2e/sign-in.cy.js
@@ -0,0 +1,39 @@
+describe('Sign in', () => {
+  beforeEach(() => {
+    cy.visit('/sign-in')
+  })
+
+  it('allows a user to sign in successfully', () => {
+    cy.get('input[name="email"]').type('test.user@fake.com');
+    cy.get('input[name="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+  
+    cy.url().should('eq', 'http://localhost:5173/preferences');
+  })
+
+  it('displays an error message when the user enters incorrect credentials', () => {
+    cy.get('input[name="email"]').type('test.user@fake.com');
+    cy.get('input[name="password"]').type('wrongPassword');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('div[role="alert"]').should('be.visible');
+  })
+
+  it('allows a user to sign out successfully', () => {
+    cy.get('input[name="email"]').type('test.user@fake.com');
+    cy.get('input[name="password"]').type('password');
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('eq', 'http://localhost:5173/preferences');
+    cy.get('button.btn-sign-out').click({ force: true});
+
+    cy.url().should('eq', 'http://localhost:5173/sign-in');
+  });
+
+  it('allows a user to get to the sign up page successfully', () => {
+    cy.get('a.sign-up-link').click();
+
+    cy.url().should('eq', 'http://localhost:5173/sign-up');
+  });
+
+});
