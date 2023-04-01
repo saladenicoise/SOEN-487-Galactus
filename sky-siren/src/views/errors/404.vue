53128f9b6c06fe27f7f942ab4d50c99e4f9e0b3c
Quoiquecesoit <90417180+Quoiquecesoit@users.noreply.github.com>
2023-04-01 01:34:45 -0400

    Styled
    The fun cube that I have been wanting to use. It changes colours on hover.

--- sky-siren/src/views/errors/404.vue
+++ sky-siren/src/views/errors/404.vue
@@ -1,6 +1,100 @@
 <template>
-    <div>
-        <h1>404</h1>
-        <p>Page not found</p>
-    </div>
+
+ <main >
+<p class="center h1 m-md-5">Page 404</p>
+	  <div class="container center">
+	  	<div class="cube">
+	  		<div style="--x:-1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:0; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  	</div>
+	  	<div class="cube">
+	  		<div style="--x:-1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:0; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  	</div><div class="cube">
+	  		<div style="--x:-1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:0; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  	</div>
+	    </div><div class="cube">
+	  		<div style="--x:-1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:0; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  		<div style="--x:1; --y:0;">
+	  			<span style="--i:3;"></span>
+	  			<span style="--i:2;"></span>
+	  			<span style="--i:1;"></span>
+	  		</div>
+	  	</div>
+	    <div class="cube">
+	    		<div style="--x:-1; --y:0;">
+	    			<span style="--i:3;"></span>
+	    			<span style="--i:2;"></span>
+	    			<span style="--i:1;"></span>
+	    		</div>
+	    		<div style="--x:0; --y:0;">
+	    			<span style="--i:3;"></span>
+	    			<span style="--i:2;"></span>
+	    			<span style="--i:1;"></span>
+	    		</div>
+	    		<div style="--x:1; --y:0;">
+	    			<span style="--i:3;"></span>
+	    			<span style="--i:2;"></span>
+	    			<span style="--i:1;"></span>
+	    		</div>
+	    	</div>
+
+    </main>
 </template>
+
+<script>
+
+</script>
+<Style>
+
+
+</Style>
