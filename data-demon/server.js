41520253b05fd0ce79f90f046e4ba502ecb7ffa6
Khaled Al K <57811573+kkodmani@users.noreply.github.com>
2023-04-01 11:47:47 -0400

    Added cache check for IP

--- data-demon/server.js
+++ data-demon/server.js
@@ -28,45 +28,21 @@ router.get("/fromIp", (req, res) => {
   let ip = search_params.get("ip"); //Gets us the requester's IP address
   let language = search_params.get("lang");
   return geoCoding.getLocationFromIp(ip).then((locationObject) => {
-    return weatherRetrieval
-      .fetchWeatherData(
-        locationObject.latitude,
-        locationObject.longitude,
-        language
-      )
-      .then((weatherData) => {
-        let jsonObject = {
-          locationObject: locationObject,
-          weatherData: weatherData,
-        };
-        let jsonString = JSON.stringify(jsonObject);
-        produce("Q1_weather", jsonString, (durable = false));
-        console.log(jsonString);
-        res.send(jsonString);
-      });
-  });
-});
-
-router.get("/fromAddress", async (req, res) => {
-  const incomingURL = new URL(req.url, `http://${req.headers.host}`);
-  const search_params = incomingURL.searchParams;
-  let cityName = search_params.get("cityName");
-  let language = search_params.get("lang");
-  return geoCoding.getLocationFromAddress(cityName).then((locationObject) => {
     if (!locationObject) {
       res.status(404).send(`Error: Could not find location ${cityName}`);
       return;
     }
+    let cityName = locationObject.city;
     client.get(`${cityName}`, async (err, data) => {
       if (err) console.log(err);
       if (data != null) {
-        console.log("[x] Available in Redis. Retreiving from cache...");
+        console.log(" [x] Available in Redis. Retreiving from cache...");
         cachedData = JSON.parse(data);
         produce("Q1_weather", cachedData, (durable = false));
         res.status(200).send(cachedData);
       } else {
         console.log(
-          "[x] Not available in Redis. Retreiving from API then caching for 1 hour..."
+          " [x] Not available in Redis. Retreiving from API then caching for 1 hour..."
         );
         return weatherRetrieval
           .fetchWeatherData(
@@ -83,7 +59,50 @@ router.get("/fromAddress", async (req, res) => {
             client.SETEX(`${cityName}`, 3600, JSON.stringify(jsonString));
             produce("Q1_weather", jsonString, (durable = false));
             res.status(200).send(jsonString);
-           // console.log(jsonString);
+            // console.log(jsonString);
+          });
+      }
+    });
+  });
+});
+
+router.get("/fromAddress", async (req, res) => {
+  const incomingURL = new URL(req.url, `http://${req.headers.host}`);
+  const search_params = incomingURL.searchParams;
+  let cityName = search_params.get("cityName");
+  let language = search_params.get("lang");
+  return geoCoding.getLocationFromAddress(cityName).then((locationObject) => {
+    if (!locationObject) {
+      res.status(404).send(`Error: Could not find location ${cityName}`);
+      return;
+    }
+    client.get(`${cityName}`, async (err, data) => {
+      if (err) console.log(err);
+      if (data != null) {
+        console.log(" [x] Available in Redis. Retreiving from cache...");
+        cachedData = JSON.parse(data);
+        produce("Q1_weather", cachedData, (durable = false));
+        res.status(200).send(cachedData);
+      } else {
+        console.log(
+          " [x] Not available in Redis. Retreiving from API then caching for 1 hour..."
+        );
+        return weatherRetrieval
+          .fetchWeatherData(
+            locationObject.latitude,
+            locationObject.longitude,
+            language
+          )
+          .then((weatherData) => {
+            let jsonObject = {
+              locationObject: locationObject,
+              weatherData: weatherData,
+            };
+            let jsonString = JSON.stringify(jsonObject);
+            client.SETEX(`${cityName}`, 3600, JSON.stringify(jsonString));
+            produce("Q1_weather", jsonString, (durable = false));
+            res.status(200).send(jsonString);
+            // console.log(jsonString);
           });
       }
     });
