77ff9aa534598086f4d7365a13955c83870a3140
Chris Di Giacomo <chrisdigiacomo13@gmail.com>
2023-04-05 12:18:16 -0400

    added svg chart generation for historical data

--- visual-voyager/server.js
+++ visual-voyager/server.js
@@ -33,73 +33,26 @@ app.get('/weeklyVisual', async(req,res) => {
 });
 
 // This following is the "generateVisual" end-point/procedure
-app.post('/historicalVisual', async (req, res) => {
+app.get('/historicalVisual', async (req, res) => {
   try {
-    const weatherData = req.body.weatherData;
-    //const historicalData = req.body.historicalData;
 
-    // Create the chart using chartjs-node-canvas
-    const width = 800;
-    const height = 600;
-    const chartCallback = (ChartJS) => {
-      // Configure the Chart.js instance here. 
-      // Useful when you need to register plugins,
-      // controllers, or other extensions 
-      // that should be available to all charts created 
-      // with the same Chart.js instance
-      ChartJS.defaults.global.defaultFontFamily = "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
-    };
-    const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height, chartCallback });
-    const chartConfiguration = {
-      type: 'line', // line chart
-      data: {
-        labels: weatherData.map(data => data.date),// An array of labels for the chart x-axis
-        datasets: [{
-          label: 'Temperature',// 
-          data: weatherData.map(data => data.temperature),// An array of data points for the chart y-axis
-          borderColor: 'rgb(255, 99, 132)', // we need to know th exact colours from the UI team so we can be in sync with the palette
-          borderWidth: 5, //its gonna be a thicc line 
-          fill: false,
-        }]
-      },
-      options: {
-        responsive: false, // guess we can actually make it interactive, assigning this false until we can test
-        title: {
-          display: true,
-          text: 'Temperature over time' //time here referes to 365 days
-        },
-        //specifying X aand Y axis of the diagram
-        scales: {
-          xAxes: [{
-            type: 'time',
-            time: {
-              unit: 'month'
-            }
-          }],
-          yAxes: [{
-            scaleLabel: {
-              display: true,
-              labelString: 'Temperature (°C)'
-            }
-          }]
-        }
-      }
-    };
+    const weatherData = require('./historicalData.json') // Loads the contents of the 'weeklyData.json' file that we will work with for testing
+    ['weatherData'] // Accesses the 'weatherData' property of the loaded JSON object
+    .filter(day => day.type === 'historical-day'); // Filters the array to only include objects with a 'type' property equal to 'forecast-day'
+    
+    //console.log(weatherData); //<--This will print the contents to the terminal
 
-    // This converts the raw binary data of the chart into a Buffer object.
-    // Buffer objects are data structures designed to encapsulate
-    // chunks of raw binary data, so that it is more convenient to work with.
-    const image = await chartJSNodeCanvas.renderToBuffer(chartConfiguration);
+    // Calls the 'createTemperatureChart' function with 'weatherData' as an argument, and waits for the Promise to resolve. The result is an image buffer representing the chart, which contains the raw data needed to construct the image, such as pixel information, color information, and image format.
+    const chart = await createTemperatureChart(weatherData); 
+    // Logs the resulting image buffer to the console for debugging purposes.
+    console.log('Generated chart image buffer:', chart);
+    //console.log('Image buffer to string: ', chart.toString('base64'));
+    
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.send(chart); // Sends the image buffer as the response body. The client will receive this as a PNG image.
+    
 
-    // Returns the base64-encoded chart image
-    // To return this image to the data service, it's convenient to convert it 
-    // to a base64-encoded string. This way, the image can be easily transmitted 
-    // over HTTP as part of the JSON response. Not only in the response from this
-    // service to the data service, but also from the data service to the UI.
-    const base64Image = image.toString('base64');
 
-    // Finally response is sent back
-    res.status(200).json({ chartImage: base64Image });
   } catch (error) {
     res.status(500).json({ error: 'Error generating chart' });
   }
@@ -112,14 +65,17 @@ app.listen(port, () => {
 });
 
 const createTemperatureChart = async (weatherData) => {
+  
+  const isForecastDay = weatherData[0].type === 'forecast-day';
 
-  const width = 650;
-  const height = 450;
+  const width = (isForecastDay ? 800 : 10000);
+  const height = (isForecastDay ? 600 : 600);
+  
   const chartJSNodeCanvas = new ChartJSNodeCanvas({ type: 'svg', width, height });
 
   const dates = weatherData.map(day => day.date);
-  const maxTemps = weatherData.map(day => day.daily_information.max_temp_c);
-  const minTemps = weatherData.map(day => day.daily_information.min_temp_c);
+  const maxTemps = weatherData.map(day => isForecastDay ? day.daily_information.max_temp_c : day.max_temp_c);
+  const minTemps = weatherData.map(day => isForecastDay ? day.daily_information.min_temp_c : day.min_temp_c);
 
   // This configuration defines a line chart with two datasets, one for maximum temperatures and one for minimum temperatures, with smooth lines and different colors for each dataset. The x-axis represents dates, and the y-axis represents temperatures in degrees Celsius. Both axes have titles displayed.
 
