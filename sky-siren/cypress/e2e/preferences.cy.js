cd6eedd2f6991fbe2fb20b8d74f61dde7a0aeac6
Jaskaran Dhadda <d.j.singh@hotmail.com>
2023-04-05 07:15:13 -0400

    Started the implementation of e2e/system tests

---
+++ sky-siren/cypress/e2e/preferences.cy.js
@@ -0,0 +1,57 @@
+describe('Preferences', () => {
+    beforeEach(() => {
+        // TODO: make it work with the store
+        cy.signIn('test.user@fake.com', 'password');
+        cy.visit('/preferences');
+        // cy.window().should('have.property', '__store__');
+        // cy.window().then( win => {
+        //     console.log(win.__store__.getters['user/userId'])
+        // });
+        
+    });
+
+    it.only('allows a user to change the language', () => {
+        cy.visit('/preferences');
+        
+        cy.get('input[name="language"]').check('fr');
+
+        cy.get('input[name="language"]').should('be.checked');
+    });
+
+    it('allows a user to change the temperature unit', () => {
+        cy.get('input[name="temperatureUnit"]').check('fahrenheit');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('input[name="temperatureUnit"]').should('be.checked');
+    });
+
+    it('allows a user to change the time format', () => {
+        cy.get('input[name="timeFormat"]').check('24');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('input[name="timeFormat"]').should('be.checked');
+    });
+
+    it('allows a user to change the location', () => {
+        cy.get('input[name="location"]').check('manual');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('input[name="location"]').should('be.checked');
+    });
+
+    it('allows a user to change the weather alerts', () => {
+        cy.get('input[name="weatherAlerts"]').check();
+        cy.get('button[type="submit"]').click();
+
+        cy.get('input[name="weatherAlerts"]').should('be.checked');
+    });
+
+    it('allows a user to change the notifications', () => {
+        cy.get('input[name="notifications"]').check();
+        cy.get('input[name="time"]').type('12:00');
+        cy.get('button[type="submit"]').click();
+
+        cy.get('input[name="notifications"]').should('be.checked');
+    });
+
+});
--- sky-siren/cypress/e2e/sign-in.cy.js
+++ sky-siren/cypress/e2e/sign-in.cy.js
@@ -8,7 +8,7 @@ describe('Sign in', () => {
     cy.get('input[name="password"]').type('password');
     cy.get('button[type="submit"]').click();
   
-    cy.url().should('eq', 'http://localhost:5173/preferences');
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
   })
 
   it('displays an error message when the user enters incorrect credentials', () => {
@@ -24,16 +24,16 @@ describe('Sign in', () => {
     cy.get('input[name="password"]').type('password');
     cy.get('button[type="submit"]').click();
 
-    cy.url().should('eq', 'http://localhost:5173/preferences');
-    cy.get('[href="/sign-out"] > .material-icons').click({ force: true});
+    cy.url().should('eq', Cypress.config().baseUrl + '/');
+    cy.get('a[href="/sign-out"]').click({ force: true});
 
-    cy.url().should('eq', 'http://localhost:5173/sign-in');
+    cy.url().should('eq', Cypress.config().baseUrl + '/sign-in');
   });
 
   it('allows a user to get to the sign up page successfully', () => {
     cy.get('a.sign-up-link').click();
 
-    cy.url().should('eq', 'http://localhost:5173/sign-up');
+    cy.url().should('eq', Cypress.config().baseUrl + '/sign-up');
   });
 
 });
