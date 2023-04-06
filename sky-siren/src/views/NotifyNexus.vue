b735f19661d42d0b2a686dbaa35de83d89dd74e4
eyeshield2110 <ndinh@hotmail.fr>
2023-04-06 07:12:39 -0400

    #23 Adding pusher beam subscription logic to the Update button, added cancel and reset buttons

--- sky-siren/src/views/NotifyNexus.vue
+++ sky-siren/src/views/NotifyNexus.vue
@@ -2,30 +2,55 @@
     <div class="notify-nexus">
         <!-- TODO Perform check to see if this devide is registed to Pusher Beam -->
         <div>
+            <h1 style="color: white">Notification service preferences</h1>
+            <input name="alertLocation" placeholder="alert" type="text" v-model="alertLocation">
+        </div>
+        <div>
             <h2 style="color: white">Notifications scheduler</h2>
             <input name="schedule" v-model="schedule" type="time">
-            <button style="color: white" @click="subscribeToNotifyNexus('scheduled-notif')">Subscribe to weather notifications?</button>
+            <button class="btn btn-outline-primary" style="color: white"
+                @click="subscribeToNotifyNexus('notification')">Subscribe to weather
+                notifications?</button>
         </div>
-
         <div>
             <h2 style="color: white">Alerts</h2>
-            <button style="color: white" @click="subscribeToNotifyNexus('weather-alert')">Subscribe to weather alerts?</button>
+            <button class="btn btn-outline-primary" style="color: white"
+                @click="subscribeToNotifyNexus('alert')">Subscribe to weather
+                alerts?</button>
+                
+        </div>
+        <!-- List of useful functions -->
+        <div>
+            <input placeholder="delete an interest" name="deleteOneInterest" v-model="deleteOneInterest" type="text">
+            <button @click="removeDeviceInterestByName(deleteOneInterest)"></button>
         </div>
 
         <div>
-            <h2 style="color: white">Remove all notifications</h2>
-            <button style="color: white" @click="unsubscribeFromAllInterests()">Remove all</button>
+            <button class="btn btn-outline-primary" style="color: white" @click="removeDeviceInterest()">remove one interest</button>
         </div>
+
+        <div>
+            <button class="btn btn-outline-primary" style="color: white" @click="getDeviceInterests()">show device interests</button>
+        </div>
+
+        <div>
+            <button class="btn btn-outline-primary" style="color: white" @click="customClearDeviceInterests()">clear all interests</button>
+        </div>
+        
+        
 
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
     data() {
         return {
-            schedule: ''
+            schedule: '',
+            deleteOneInterest: '',
+            alertLocation: ''
         }
     },
     methods: {
@@ -34,55 +59,112 @@ export default {
 
             // Registers and subscribes to "weather-alert"
             const beamsClient = new PusherPushNotifications.Client({
-            instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
+                instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
             });
-        
+            console.log('schedule debug: ', this.schedule)
             beamsClient.start()
-            .then((beamsClient) => beamsClient.getDeviceId())
-            .then(async (deviceId) => {
-                console.log(`Successfully registered device ${deviceId} and subscribed to ${interest}!`)
-                // TODO Register deviceId in User service, if does not exist yet
-                // TODO Refactor into switch case?
-                if (interest === 'weather-alert')
-                    await this.postDeviceIdToNotifyNexus(deviceId).catch(e => console.log(`Can't send alert deviceId to notify-nexus service: ${e}`));
-                if (interest === 'scheduled-notif')
-                    await this.postNotificationPreferences(deviceId, this.schedule).catch(e => console.log(`Can't send notification preferences to notify-nexus service: ${e}`));
-            })
-            .then(() => beamsClient.addDeviceInterest(interest))
-            // TO DELETE AFTER PR
-            .then(() => beamsClient.addDeviceInterest('alert-montreal'))
-            .then(() => beamsClient.addDeviceInterest('alert-brisbane'))
-            .then(() => beamsClient.addDeviceInterest('alert-london'))
-            // ^ 
-            .then(() => beamsClient.getDeviceInterests())
-            .then((interests) => console.log(`Current interests: ${interests}`))
-            .catch(console.error);
+                .then((beamsClient) => beamsClient.getDeviceId())
+                .then(async (deviceId) => {
+                    console.log(`Successfully registered device ${deviceId} and subscribed to ${interest}!`)
+                    if (interest === 'alert')
+                        await this.postDeviceIdToNotifyNexus(deviceId)
+                        .catch(e => console.log(`Can't send alert deviceId to notify-nexus service: ${e}`));
+                    if (interest === 'notification')
+                        await this.postNotificationPreferences(deviceId, this.schedule)
+                        .catch(e => console.log(`Can't send notification preferences to notify-nexus service: ${e}`));
+                })
+                // interest naming convention: alert-<location> and notification-<location>-<schedule>
+                .then(() => {
+                    let newInterest = interest + '-' + this.alertLocation;
+                    if (interest === 'notification')
+                    {
+                            // parsing time from hh:mm format to hhHmm since char ':' is not allowed by pusher-beam
+                            newInterest +='-' + this.schedule;
+                            newInterest = newInterest.replace(':', 'h');
+                    }
+                    console.log(newInterest);
+                    beamsClient.addDeviceInterest(newInterest)
+                })
+                .catch(console.error);
         },
-        unsubscribeFromAllInterests() {
+        getDeviceInterests() {
+            // this function works
             const beamsClient = new PusherPushNotifications.Client({
-            instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
+                instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
             });
-            beamsClient.clearDeviceInterests()
-                .then()
-                .catch(e => console.log(e));
-            beamsClient.getDeviceInterests()
-                .then((interests) => console.log(`Current interests after clear: ${interests}`))
-                .catch(error => console.log(error));
+            beamsClient.start()
+                .then((beamsClient) => beamsClient.getDeviceInterests())
+                .then((interests) => console.table(interests))
+                .catch((err) => console.log(err))
         },
+        async getDeviceId() {
+            const beamsClient = new PusherPushNotifications.Client({
+                instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
+            });
+
+            beamsClient.start()
+                .then(() => beamsClient.getDeviceId())
+                .then(deviceId => {
+                    console.log(deviceId) // Will log something like web-1234-1234-1234-1234
+                })
+                .catch(e => console.error('Could not get device id', e));
+        },
+
+
+        removeDeviceInterest() {
+            console.log('removeDeviceInterest')
+            const beamsClient = new PusherPushNotifications.Client({
+                instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
+            });
+            beamsClient.start()
+                .then((beamsClient) => beamsClient.removeDeviceInterest('alert-brisbane'))
+                .catch(e => console.error('Could not remove device interest', e));
+        },
+
+        removeDeviceInterestByName(name) {
+            // This function works
+            console.log('removeDeviceInterest')
+            const beamsClient = new PusherPushNotifications.Client({
+                instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
+            });
+            beamsClient.start()
+                .then((beamsClient) => beamsClient.removeDeviceInterest(name))
+                .catch(e => console.error('Could not remove device interest', e));
+        },
+
+        customClearDeviceInterests() {
+            // This function works
+            console.log('customClearDeviceInterests');
+            // get current device's interests
+            const beamsClient = new PusherPushNotifications.Client({
+                instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb',
+            });
+            beamsClient.start()
+                .then((beamsClient) => beamsClient.getDeviceInterests())
+                .then((interests) => interests.forEach((interest) => 
+                    beamsClient.removeDeviceInterest(interest))
+                )
+                .then(() => console.log('Unsubscribe from all interests'))
+                .catch((err) => console.log(err))
+
+        },
+
+
         async postNotificationPreferences(deviceId, schedule) {
             let res = await axios.post("http://localhost:3001/post/notification-preferences", {
                 deviceId,
                 schedule
-            }).catch(e =>  console.log(`weird axios or await error? ${e}`))
+            }).catch(e => console.log(`weird axios or await error? ${e}`))
             console.log(`postNotificationPreferences result: ${res.data}`);
         },
         // This function calls the notify-nexus server to pass it the registered device id
         async postDeviceIdToNotifyNexus(deviceId) {
             let res = await axios.post("http://localhost:3001/post/deviceId", {
                 deviceId
-            }).catch(e =>  console.log(`weird axios or await error? ${e}`))
+            }).catch(e => console.log(`weird axios or await error? ${e}`))
             console.log(`postDeviceIdToNotifyNexus result: ${res.data}`);
         }
+
     }
 }
 </script>
--- sky-siren/src/views/user/Preferences.vue
+++ sky-siren/src/views/user/Preferences.vue
@@ -19,15 +19,18 @@
             <div class="mb-4">
                 <label class="form-label">Temperature unit:&nbsp;</label>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="temperatureUnitCelsius" value="celsius" v-model="temperatureUnit">
+                    <input class="form-check-input" type="radio" id="temperatureUnitCelsius" value="celsius"
+                        v-model="temperatureUnit">
                     <label class="form-check-label" for="temperatureUnitCelsius">Celsius</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="temperatureUnitFahrenheit" value="fahrenheit" v-model="temperatureUnit">
+                    <input class="form-check-input" type="radio" id="temperatureUnitFahrenheit" value="fahrenheit"
+                        v-model="temperatureUnit">
                     <label class="form-check-label" for="temperatureUnitFahrenheit">Fahrenheit</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="temperatureUnitKelvin" value="kelvin" v-model="temperatureUnit">
+                    <input class="form-check-input" type="radio" id="temperatureUnitKelvin" value="kelvin"
+                        v-model="temperatureUnit">
                     <label class="form-check-label" for="temperatureUnitKelvin">Kelvin</label>
                 </div>
             </div>
@@ -45,32 +48,57 @@
             <div class="mb-4">
                 <label class="form-label">Location:&nbsp;</label>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="locationAutoDetect" value="autoDetect" v-model="location">
+                    <input class="form-check-input" type="radio" id="locationAutoDetect" value="autoDetect"
+                        v-model="location">
                     <label class="form-check-label" for="locationAutoDetect">Auto Detect</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" id="locationManualInput" value="manualInput" v-model="location">
+                    <input class="form-check-input" type="radio" id="locationManualInput" value="manualInput"
+                        v-model="location">
                     <label class="form-check-label" for="locationManualInput">Manual Input</label>
                 </div>
             </div>
             <div class="mb-4">
+                <div class="form-text">
+                    <input class="form-text-input" type="text" id="city" v-model="city" :disabled="location === 'autoDetect'">
+                    <label class="form-text-label" for="city">Location manual input</label>
+                </div>
+            </div>
+            <div class="mb-4">
                 <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="weatherAlerts" v-model="weatherAlerts">
+                    <input class="form-check-input" type="checkbox" role="switch" id="weatherAlerts"
+                        v-model="weatherAlerts">
                     <label class="form-check-label" for="weatherAlerts">Weather alerts</label>
                 </div>
             </div>
+
             <div class="mb-4">
                 <div class="form-check form-switch">
                     <input class="form-check-input" type="checkbox" role="switch" id="notification" v-model="notification">
                     <label class="form-check-label" for="notification">Notification alerts</label>
                 </div>
                 <div class="">
-                    <input type="time" class="form-control" v-model="notificationSchedule"/>
+                    <input type="time" class="form-control" v-model="notificationSchedule" />
                 </div>
             </div>
 
             <button class="btn btn-outline-primary" type="submit">Update</button>
+            <button class="btn btn-outline-danger" type="button" @click="cancelPreferenceUpdate()">Cancel</button>
+            <button class="btn btn-outline-info" type="button" @click="resetPreference()">Reset</button>
         </form>
+
+        <!-- <div>
+            <h3>Debugging utility functions. Comment this out</h3>
+            <div>
+                <button class="btn btn-outline-primary" @click="getDeviceInterests()">Get interests</button>
+            </div>
+            <div>
+                <button class="btn btn-outline-primary" @click="customClearDeviceInterests()">Delete all interests</button>
+            </div>
+            <div>
+                <button class="btn btn-outline-primary" @click="getLocationSelected()">Get location selected</button>
+            </div>
+        </div> -->
     </main>
 </template>
 
@@ -80,8 +108,10 @@
 import { ref, onMounted } from 'vue';
 import router from '@/router';
 import { useStore } from 'vuex';
+import axios from 'axios'
 
 /* Data */
+const city = ref('')
 const language = ref('');
 const temperatureUnit = ref('');
 const timeFormat = ref('');
@@ -92,10 +122,16 @@ const notificationSchedule = ref('');
 const errorMsg = ref('');
 const store = useStore();
 
+
+// Pusher beam client sdk
+const beamsClient = new PusherPushNotifications.Client({ instanceId: 'b8e54c3c-7ad0-41ce-9c6f-63f5d8f52dcb', });
+
+
 /* Once the component is mounted */
 onMounted(() => {
     const userData = store.getters['user/userData'];
     if (userData) {
+        city.value = userData.city;
         language.value = userData.language;
         temperatureUnit.value = userData.temperatureUnit;
         timeFormat.value = userData.timeFormat;
@@ -108,9 +144,59 @@ onMounted(() => {
 
 
 /* Methods */
+const cancelPreferenceUpdate = async () => {
+    // cancels ongoing updates
+
+    // copy of the onMounted code
+    const userData = store.getters['user/userData'];
+    if (userData) {
+        city.value = userData.city;
+        language.value = userData.language;
+        temperatureUnit.value = userData.temperatureUnit;
+        timeFormat.value = userData.timeFormat;
+        location.value = userData.location;
+        weatherAlerts.value = userData.weatherAlerts;
+        notification.value = userData.notification;
+        notificationSchedule.value = userData.notificationSchedule;
+    } else {
+        // reset form to blank slate
+        city.value = '';
+        language.value = '';
+        temperatureUnit.value = '';
+        timeFormat.value = '';
+        location.value = '';
+        weatherAlerts.value = false;
+        notification.value = false;
+        notificationSchedule.value = '';
+    }
+}
+
+const resetPreference = async () => {
+    city.value = '';
+        language.value = '';
+        temperatureUnit.value = '';
+        timeFormat.value = '';
+        location.value = '';
+        weatherAlerts.value = false;
+        notification.value = false;
+        notificationSchedule.value = '';
+}
+
 const updatePreference = async () => {
+    // delete all current pusher beam interests
+    customClearDeviceInterests();
+
+    // add new ones according to preferences here
+    // update the alerts
+    if (weatherAlerts.value)
+        addInterest('alert');
+    // update the scheduled notification
+    if (notification.value)
+        addInterest('notification');
+
     try {
         await store.dispatch('user/updateUserData', {
+            city: city.value,
             language: language.value,
             temperatureUnit: temperatureUnit.value,
             timeFormat: timeFormat.value,
@@ -119,10 +205,73 @@ const updatePreference = async () => {
             notification: notification.value,
             notificationSchedule: notificationSchedule.value,
         });
-        router.push('/');
+        router.push('/preferences');
     } catch (error) {
         errorMsg.value = error.message;
     }
 };
+// TODO
+// Add call to data-service when Location is updated to autoDetect -> /fromIP
+// The city input is disabled but its value is changed to the current city
+
+// What if the cityis already auto-detected (saved in db), but user changed city so the auto detect input is "outdated" until we do -> manual input -> auto detect
+// .. upon which, the location will be update to current location
+
+// Pusher beam integration methods
+const onChangeToAutoDetect = () => {
+
+}
+
+const onUpdateAutoDetectValue = () => {
+    
+}
+
+const getDeviceInterests = () => {
+    beamsClient.start()
+        .then((beamsClient) => beamsClient.getDeviceInterests())
+        .then((interests) => console.table(interests))
+        .catch((err) => console.log(err))
+}
+
+const removeDeviceInterestByName = (name) => {
+    // This function works
+    console.log('removeDeviceInterest')
+    beamsClient.start()
+        .then((beamsClient) => beamsClient.removeDeviceInterest(name))
+        .catch(e => console.error('Could not remove device interest', e));
+}
+
+const customClearDeviceInterests = () => {
+    // This function works
+    console.log('customClearDeviceInterests');
+    // get current device's interests
+    beamsClient.start()
+        .then((beamsClient) => beamsClient.getDeviceInterests())
+        .then((interests) => interests.forEach((interest) =>
+            beamsClient.removeDeviceInterest(interest))
+        )
+        .then(() => console.log('Unsubscribe from all interests'))
+        .catch((err) => console.log(err))
+}
+
+const addInterest = (interest) => {
+    console.log(`Subscribe to ${interest} interest for notify-nexus`);
+
+    beamsClient.start()
+        .then(() => {
+            let newInterest = interest + '-' + city.value;
+            if (interest === 'notification') {
+                // parsing time from 08:55 format to 08h55 since char ':' is not allowed by pusher-beam
+                newInterest += '-' + notificationSchedule.value;
+                newInterest = newInterest.replace(':', 'h');
+            }
+            if (interest === 'notification' || interest === 'alert') {
+                console.log(newInterest);
+                beamsClient.addDeviceInterest(newInterest);
+            }
+
+        })
+        .catch(console.error);
+}
 
 </script>
