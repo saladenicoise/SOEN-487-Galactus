664f2d8277cbe5fa1781415eb074a1fba56bc4f7
Jaskaran Dhadda <d.j.singh@hotmail.com>
2023-04-07 20:54:32 -0400

    Removed unused dependencies, installed the missing one

--- sky-siren/src/main.js
+++ sky-siren/src/main.js
@@ -4,11 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
 import './assets/main.css'
-
 
 const app = createApp(App)
 
