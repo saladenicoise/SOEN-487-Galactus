14bef9e609972be72af92d5bc00f05e5dbaec1bb
Jaskaran Dhadda <d.j.singh@hotmail.com>
2023-04-09 23:56:44 -0400

    Added heart to the footer

--- sky-siren/src/components/TheFooter.vue
+++ sky-siren/src/components/TheFooter.vue
@@ -1,17 +1,13 @@
 <template>
-<footer  id="footer">
-<!-- bi-heart-fill is the heart icon from bootstrap-icons-->
-		<span class="text-muted"> Made with <i class="bi bi-heart-fill text-danger"></i> by L'equipe<br> </span>
-
-		<span class="text-muted">&copy; {{ new Date().getFullYear() }} All Rights Reserved.</span>
-
-
-</footer>
+  <footer id="footer">
+    <span class="text-muted"> Made with ❤ by L'equipe<br> </span>
+    <span class="text-muted">&copy; {{ new Date().getFullYear() }} All Rights Reserved.</span>
+  </footer>
 </template>
 
 <style scoped>
 #footer {
-text-align: center;
+  text-align: center;
   position: absolute;
   bottom: 0%;
   color: white;
@@ -22,8 +18,6 @@ text-align: center;
   padding: 0.5%;
   width: 100%;
   align-content: center;
-  height: 4rem;            /* Footer height */
-
+  height: 4rem;
 }
-
 </style>
