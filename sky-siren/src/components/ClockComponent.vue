4c35372cb2d6a338a89f7751bc649a1ac589ee5c
Jaskaran Dhadda <d.j.singh@hotmail.com>
2023-04-10 18:05:12 -0400

    Refactored the code

--- sky-siren/src/components/ClockComponent.vue
+++ sky-siren/src/components/ClockComponent.vue
@@ -1,96 +1,86 @@
 <template>
-
-
-      <div id="clock">
-         <div class="time">{{ time }}</div>
-         <div class="date">{{ date }}</div>
-
-      </div>
-
+   <div id="clock">
+      <div class="theTime">{{ theTime }}</div>
+      <div class="theDate">{{ theDate }}</div>
+   </div>
 </template>
 
-<script>
-export default {
-   name: 'ClockComponent',
-   data() {
-      return {
-         date: '',
-         time: ''
-      }
-   },
-   mounted() {
-      this.getTime();
-      setInterval(this.getTime, 1000);
-   },
-   methods: {
-      getTime() {
-         let date = new Date();
-         this.date = date.toLocaleDateString();
-         this.time = date.toLocaleTimeString();
-      }
-   },
-}
+<script setup>
+import { ref, onMounted } from "vue";
 
+const theTime = ref("");
+const theDate = ref("");
+
+onMounted(() => {
+   getTime();
+   setInterval(getTime, 1000);
+});
+
+const getTime = () => {
+   let todayDate = new Date();
+   theDate.value = todayDate.toLocaleDateString();
+   theTime.value = todayDate.toLocaleTimeString();
+}
 </script>
+
 <style scoped>
 #clockmain {
-  box-sizing: border-box;
+   box-sizing: border-box;
 }
 
- #clock {
-  font-family: "Share Tech Mono", monospace;
+#clock {
+   font-family: "Share Tech Mono", monospace;
    position: absolute;
 
-    padding: 10.9px 10.9px;
-    margin: 0;
-    display: flex;
-    box-sizing:border-box;
-    Background: linear-gradient(360deg, rgba(23, 31, 43, 1) 20%, rgba(32, 46, 60, 1) 50%, rgba(39, 57, 72, 1) 60%, rgba(37, 56, 68, 1) 70%, rgba(36, 59, 85, 1) 80%, rgba(23, 31, 43, 1) 95%);
-    border: 1px groove var(--dark);
-    border-width: 10px;
-    border-radius: 25px;
-    font-family: verdana;
-    font-size: 16px;
-    z-index: 1;
-  top:10vh;
-  left: 20vw;
-    transform: translate(-50%, -50%);
-    background-clip: border-box;
+   padding: 10.9px 10.9px;
+   margin: 0;
+   display: flex;
+   box-sizing: border-box;
+   Background: linear-gradient(360deg, rgba(23, 31, 43, 1) 20%, rgba(32, 46, 60, 1) 50%, rgba(39, 57, 72, 1) 60%, rgba(37, 56, 68, 1) 70%, rgba(36, 59, 85, 1) 80%, rgba(23, 31, 43, 1) 95%);
+   border: 1px groove var(--dark);
+   border-width: 10px;
+   border-radius: 25px;
+   font-family: verdana;
+   font-size: 16px;
+   z-index: 1;
+   top: 10vh;
+   left: 20vw;
+   transform: translate(-50%, -50%);
+   background-clip: border-box;
 
-   }
+}
 
-   #clock .time {
+#clock .theTime {
 
-      letter-spacing: 0.05em;
-      font-size: 32px;
-      text-shadow: 0 0 10px #0a9ecf, 0 0 10px rgba(7, 42, 90, 0.834);
-      color: #03e9f4;
+   letter-spacing: 0.05em;
+   font-size: 32px;
+   text-shadow: 0 0 10px #0a9ecf, 0 0 10px rgba(7, 42, 90, 0.834);
+   color: #03e9f4;
 
 
 }
-#clock .date {
-  letter-spacing: 0.1em;
-  font-size: 14px;
-  text-shadow: 0 0 10px #0a9ecf, 0 0 10px rgba(7, 42, 90, 0.834);
-      color: #03e9f4;
+
+#clock .theDate {
+   letter-spacing: 0.1em;
+   font-size: 14px;
+   text-shadow: 0 0 10px #0a9ecf, 0 0 10px rgba(7, 42, 90, 0.834);
+   color: #03e9f4;
 }
 
 
 @keyframes textColor {
-    0% {
+   0% {
       color: var(--light);
-    }
+   }
 
-    50% {
+   50% {
       color: var(--grey);
-    }
+   }
 
-75%{
+   75% {}
 
+   100% {
+      color: var(--light);
+   }
 }
-    100% {
-color: var(--light);
-    }
-  }
-
 </style>
--- sky-siren/src/views/AboutPage.vue
+++ sky-siren/src/views/AboutPage.vue
@@ -1,99 +1,137 @@
 <template>
   <main id="mainAboutPage">
     <div>
-      <h1>[ L'équipe ]</h1><br>
-      || 40110945 | Jules Petit<br>
-      || 40110044 |Elias Sabbagh <br>
-      || 40045833 | Jaskaran Dhadda <br>
-      || 40133600 | Christopher Di Giacomo<br>
-      || 40099828 | Carolina Quesada Vilchez<br>
-      || 29199160 | Khaled Al Koudmani<br>
-      || 40158506 | Mikhail Ovsyannikov<br>
-      || 40128079 | Noah-James Dinh<br>
-      || 40116096 | Rama Alrifai<br><br>
+      <h1>L'équipe</h1>
+    
+      <div v-for="member in teamMembers" :key="member.studentId">
+        {{ member.name }} - {{ member.studentId }} - <a :href="'https://github.com/' + member.githubUsername">{{ member.githubUsername }}</a>
+      </div>
     </div>
   </main>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from 'vue';
 
+const teamMembers = ref([
+  {
+    name: 'Jules Petit',
+    studentId: '40110945',
+    githubUsername: 'saladenicoise',
+  },
+  {
+    name: 'Elias Sabbagh',
+    studentId: '40110044',
+    githubUsername: 'EliasSabbagh47',
+  },
+  {
+    name: 'Jaskaran Dhadda',
+    studentId: '40045833',
+    githubUsername: 'jaskaran-dhadda',
+  },
+  {
+    name: 'Christopher Di Giacomo',
+    studentId: '40133600',
+    githubUsername: 'chrisdigiacs',
+  },
+  {
+    name: 'Carolina Quesada Vilchez',
+    studentId: '40099828',
+    githubUsername: 'Quoiquecesoit',
+  },
+  {
+    name: 'Khaled Al Koudmani',
+    studentId: '29199160',
+    githubUsername: 'kkodmani',
+  },
+  {
+    name: 'Mikhail Ovsyannikov',
+    studentId: '40158506',
+    githubUsername: 'allfatherWednesday',
+  },
+  {
+    name: 'Noah-James Dinh',
+    studentId: '40128079',
+    githubUsername: 'eyeshield2110',
+  },
+  {
+    name: 'Rama Alrifai',
+    studentId: '40116096',
+    githubUsername: 'riri404',
+  }
+]);
+</script>
+
+<style scoped>
 #mainAboutPage {
-  margin: 0;
   padding: 0;
-  background: url("https://static.pexels.com/photos/1227/night-dark-blur-blurred.jpg") no-repeat center center;
+  background: url("/blurred.jpg") no-repeat center center;
   background-size: cover;
   width: 100vw;
   height: 100vh;
   opacity: 0.9;
   z-index: -100;
 
-   }
-
-
-  #mainAboutPage div {
-    position: relative;
-  height: fit-content;
-width: fit-content;
-    font-size: 2vw;
-     line-height: 150%;
-    font-family: 'Roboto', sans-serif;
-    font-weight: 600;
-    font-size: xx-large;
-    text-align: center;
-    margin: auto;
-    overflow: hidden;
-    box-shadow: #151823fa 0px 40px 40px 40px;
-    background: #151823;
-    animation: animateGlow 10s ease infinite;
-    animation: textColor 10s ease infinite;
-  }
-
-  #mainAboutPage div:after {
-    position: relative;
-    content: "";
-    top: 5vw;
-    left: 0;
-    right: 0;
-    z-index: -1;
-    height: 100%;
-    width: 100%;
-    margin: 0 auto;
-    transform: scale(1);
-  }
-
-  @keyframes animateGlow {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
-
-  @keyframes textColor {
-    0% {
-      color: #00a68a;
-    }
-    25%{
-
-      color: #83d47d;
-
-
-    }
-
-    50% {
-      color:#007480
-    }
-
-75%{
-  color: #9aae95;
-
 }
-    100% {
-color:#00a68a
-    }
+
+#mainAboutPage div {
+  position: relative;
+  height: fit-content;
+  width: fit-content;
+  font-size: 2vw;
+  line-height: 150%;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 600;
+  font-size: x-large;
+  text-align: center;
+  margin: auto;
+  overflow: hidden;
+  box-shadow: #151823fa 0px 40px 40px 40px;
+  background: #151823;
+  animation: animateGlow 10s ease infinite;
+  animation: textColor 10s ease infinite;
+}
+
+#mainAboutPage div:after {
+  position: relative;
+  content: "";
+  top: 5vw;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  transform: scale(1);
+}
+
+@keyframes animateGlow {
+  0% {
+    background-position: 0% 50%;
   }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+@keyframes textColor {
+  0% {
+    color: #00a68a;
+  }
+  25% {
+    color: #83d47d;
+  }
+  50% {
+    color: #007480
+  }
+  75% {
+    color: #9aae95;
+  }
+  100% {
+    color: #00a68a
+  }
+}
 </style>
