13bfd77eab07f24142dbe1ac4abc80c6a127b483
Jaskaran Dhadda <d.j.singh@hotmail.com>
2023-04-09 23:56:28 -0400

    Added missing pusher script

--- sky-siren/src/main.js
+++ sky-siren/src/main.js
@@ -1,15 +1,17 @@
 import { createApp } from 'vue'
-
+import { loadScript } from 'vue-plugin-load-script'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-
 import './assets/main.css'
-
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
 app.mount('#app')
+
+// necessary for the Notification service to work
+// please don't delete this. not again.
+loadScript("https://js.pusher.com/beams/1.0/push-notifications-cdn.js")
